96f93b3cbd6c020d29f6987b7b17079db22d2193
Abhishek Vishwakarma <52538014+abhigdrv@users.noreply.github.com>
2023-10-13 09:44:10 +0530

    feat: added directive guard and fixed config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { ChangeBgColorOnClickDirective } from './directives/change-bg-color-on-click.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChangeBgColorOnClickDirective
   ],
   imports: [
     BrowserModule,
